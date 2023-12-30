da6dc0db1ecde625f26616ee288d29ab708ef0ce
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2023-12-29 19:00:42 -0500

    Delete clase 14 -20 dic- filtros automoviles directory

--- clase 14 -20 dic- filtros automoviles/js/animation.js
+++
@@ -1,9 +0,0 @@
-const banner = document.querySelector(".banner");
-
-setInterval(function () {
-  banner.style.backgroundImage = "url(img/bg-light.jpg)";
-
-  setTimeout(function () {
-    banner.style.backgroundImage = "url(img/bg.jpg)";
-  }, 1000);
-}, 2000);
